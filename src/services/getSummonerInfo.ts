ff2bf6d33b95f6a1f408054dc9aa846d935ec9ae
HitsujiRere <hitsujirere@gmail.com>
2025-10-28 21:55:12 +0900

    format: safeTryを用いた簡素化

--- src/services/getSummonerInfo.ts
+++ src/services/getSummonerInfo.ts
@@ -1,5 +1,5 @@
 import type { ContentfulStatusCode } from "hono/utils/http-status";
-import { err, ok, type Result } from "neverthrow";
+import { err, ok, type ResultAsync, safeTry } from "neverthrow";
 import { createRiotApi } from "@/infrastructures/riot";
 import { joinRank, type Rank } from "@/types/rank";
 import { parseRiotId } from "@/types/riotId";
@@ -15,46 +15,40 @@ type GetSummonerInfoError = {
   status: ContentfulStatusCode;
 };
 
-export const getSummonerInfo = async (
+export const getSummonerInfo = (
   name: string,
-): Promise<Result<SummonerInfo, GetSummonerInfoError>> => {
-  const riotId = parseRiotId(name);
-  if (riotId === undefined) {
-    return err({
-      message: "Parameter 'id' is not in the correct format.",
-      status: 400,
+): ResultAsync<SummonerInfo, GetSummonerInfoError> => {
+  return safeTry(async function* () {
+    const riotId = parseRiotId(name);
+    if (riotId === undefined) {
+      return err({
+        message: "Parameter 'id' is not in the correct format.",
+        status: 400,
+      } satisfies GetSummonerInfoError);
+    }
+
+    const riotApi = await createRiotApi();
+
+    const puuid = yield* (await riotApi.getPuuid(riotId)).safeUnwrap();
+
+    const summonerDTO = yield* (
+      await riotApi.getSummonerDTO(puuid)
+    ).safeUnwrap();
+
+    const leagueEntriesDTO = yield* (
+      await riotApi.getLeagueEntriesDTO(puuid)
+    ).safeUnwrap();
+    const soloRanked = leagueEntriesDTO.find(
+      (leagueEntry) => leagueEntry.queueType === "RANKED_SOLO_5x5",
+    );
+    const soloRankedRank: Rank = soloRanked
+      ? joinRank(soloRanked.tier, soloRanked.rank)
+      : "UNRANKED";
+
+    return ok({
+      summonerLevel: summonerDTO.summonerLevel,
+      profileIconId: summonerDTO.profileIconId,
+      soloRankedRank,
     });
-  }
-
-  const riotApi = await createRiotApi();
-
-  const puuidResult = await riotApi.getPuuid(riotId);
-  if (puuidResult.isErr()) {
-    return err(puuidResult.error);
-  }
-  const puuid = puuidResult.value;
-
-  const summonerDTOResult = await riotApi.getSummonerDTO(puuid);
-  if (summonerDTOResult.isErr()) {
-    return err(summonerDTOResult.error);
-  }
-  const summonerDTO = summonerDTOResult.value;
-
-  const leagueEntriesDTOResult = await riotApi.getLeagueEntriesDTO(puuid);
-  if (leagueEntriesDTOResult.isErr()) {
-    return err(leagueEntriesDTOResult.error);
-  }
-  const leagueEntriesDTO = leagueEntriesDTOResult.value;
-  const soloRanked = leagueEntriesDTO.find(
-    (leagueEntry) => leagueEntry.queueType === "RANKED_SOLO_5x5",
-  );
-  const soloRankedRank: Rank = soloRanked
-    ? joinRank(soloRanked.tier, soloRanked.rank)
-    : "UNRANKED";
-
-  return ok({
-    summonerLevel: summonerDTO.summonerLevel,
-    profileIconId: summonerDTO.profileIconId,
-    soloRankedRank,
   });
 };
