b2adf78c805741d5e4d22c1e6245d4247e47040c
HitsujiRere <hitsujirere@gmail.com>
2025-10-28 23:05:44 +0900

    feat: DEBUGサモナーのランクをランダムに返すように

--- src/server/infrastructures/riot/getLeagueEntriesDTO.ts
+++ src/server/infrastructures/riot/getLeagueEntriesDTO.ts
@@ -7,6 +7,8 @@ import z from "zod";
 import { logger } from "@/server/logger";
 import { QUEUES } from "@/types/queue";
 import { RANK_NUMBERS, TIERS } from "@/types/rank";
+import { choice } from "@/utils/choice";
+import { randomBetween } from "@/utils/random";
 import { safeFetch } from "../utils/safeFetch";
 import { safeZodParse } from "../utils/safeZodParse";
 
@@ -39,7 +41,21 @@ export const getLeagueEntriesDTO = (
   puuid: string,
 ): ResultAsync<LeagueEntriesDTO, GetLeagueEntriesDTOError> => {
   if (puuid === "debug_puuid") {
-    return okAsync([]);
+    return okAsync([
+      {
+        leagueId: "debug_league_id",
+        queueType: "RANKED_SOLO_5x5",
+        tier: choice(TIERS),
+        rank: choice(RANK_NUMBERS),
+        leaguePoints: randomBetween(0, 99),
+        wins: randomBetween(1, 100),
+        losses: randomBetween(1, 100),
+        veteran: choice([true, false]),
+        inactive: choice([true, false]),
+        freshBlood: choice([true, false]),
+        hotStreak: choice([true, false]),
+      },
+    ]);
   }
 
   return safeFetch(
@@ -47,6 +63,7 @@ export const getLeagueEntriesDTO = (
     { headers: { "X-Riot-Token": riotApiKey } },
   )
     .andThen(safeZodParse(schema))
+    .andTee((entries) => console.log(entries))
     .mapErr((error): GetLeagueEntriesDTOError => {
       logger.error(error);
       return {
