d0763f1c031ff0e49b19aec6fc29fff573baa860
HitsujiRere <hitsujirere@gmail.com>
2025-10-30 15:46:53 +0900

    chore: tsconfigのtargetをesnextに変更 refactor: parseRiotIdで名前付きキャプチャを使うように

--- src/types/riotId/parse.ts
+++ src/types/riotId/parse.ts
@@ -3,12 +3,13 @@ import type { RiotId } from "./riotId";
 export const parseRiotId = (formatted: string): RiotId | undefined => {
   const res = formatted
     .trim()
-    .match(/^\u2066*(.+?)[ \u2069]*#\u2066*(.+?)\u2069*$/);
+    .match(/^\u2066*(?<name>.+?)[ \u2069]*#\u2066*(?<tag>.+?)\u2069*$/);
   if (res === null) {
     return undefined;
   }
 
-  const [, gameName, tagLine] = res;
+  const gameName = res.groups?.name;
+  const tagLine = res.groups?.tag;
   if (gameName === undefined || tagLine === undefined) {
     return undefined;
   }
