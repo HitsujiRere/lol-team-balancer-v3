7eec62d12ff082d347e369125e635170bbda1007
HitsujiRere <hitsujirere@gmail.com>
2026-01-17 03:04:08 +0900

    feat: importmetaの定義

--- src/react-app/vite-env.d.ts
+++ src/react-app/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  /**
+   * League of Legends DDRAGON Version
+   */
+  readonly VITE_DDRAGON_VERSION: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
