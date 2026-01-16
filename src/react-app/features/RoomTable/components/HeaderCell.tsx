eb438b44b98fcfa3cc97e62417de6a8f86925946
HitsujiRere <hitsujirere@gmail.com>
2026-01-17 01:40:16 +0900

    feat: OP.GGマルチサーチリンク追加

--- src/react-app/features/RoomTable/components/HeaderCell.tsx
+++ src/react-app/features/RoomTable/components/HeaderCell.tsx
@@ -1,7 +1,11 @@
-import { Checkbox } from "@heroui/react";
+import { Checkbox, Link } from "@heroui/react";
 import { useAtom, useAtomValue } from "jotai";
+import { useAtomCallback } from "jotai/utils";
+import { useCallback } from "react";
+import { toOpggMultisearchLink } from "#domain/riotId";
 import { roomAtom } from "../../../stores/room";
 import { selectionAtom } from "../../../stores/selection";
+import { summonerFamily } from "../../../stores/summoner";
 
 export const columns = [
   { name: "選択", uid: "select" },
@@ -11,14 +15,18 @@ export const columns = [
   { name: "聞き専", uid: "isMute" },
 ];
 
+const useRiotIds = () =>
+  useAtomCallback(
+    useCallback((get, _set, names: string[]) => {
+      return names.map((name) => get(summonerFamily(name)).riotId);
+    }, []),
+  );
+
 export const HeaderCell = ({ name, uid }: { name: string; uid: string }) => {
   const room = useAtomValue(roomAtom);
   const [selection, setSelection] = useAtom(selectionAtom);
 
-  const selects = room.reduce(
-    (count, cur) => count + (selection[cur] === "selected" ? 1 : 0),
-    0,
-  );
+  const getRiotIds = useRiotIds();
 
   const handleChange = (isSelected: boolean) => {
     setSelection((selection) => {
@@ -29,12 +37,36 @@ export const HeaderCell = ({ name, uid }: { name: string; uid: string }) => {
   };
 
   if (uid === "select") {
+    const selects = room.reduce(
+      (count, cur) => count + (selection[cur] === "selected" ? 1 : 0),
+      0,
+    );
+
     return (
       <Checkbox
         isSelected={selects === room.length}
         isIndeterminate={1 <= selects && selects < room.length}
         onValueChange={handleChange}
       />
+    );
+  }
+
+  if (uid === "name") {
+    const top10RiotIds = getRiotIds(room)
+      .filter((id) => !!id)
+      .slice(0, 10);
+
+    return (
+      <div className="flex items-center gap-4">
+        <div>{name}</div>
+        <Link
+          isExternal
+          href={toOpggMultisearchLink(top10RiotIds)}
+          className="text-sm"
+        >
+          <span className="font-mono">OP.GG</span> マルチサーチ
+        </Link>
+      </div>
     );
   }
 
